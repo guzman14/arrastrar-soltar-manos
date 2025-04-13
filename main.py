054f4dba86e37a08308ddfa80938dd8f19de7f70
Mario Guzman <mguzmanr8@miumg.edu.gt>
2025-04-12 19:05:45 -0600

    Agregar archivo main.py con detección de manos

---
+++ main.py
@@ -0,0 +1,24 @@
+# main.py
+import cv2
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils
+
+while True:
+    success, img = cap.read()
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(img_rgb)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_draw.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+    cv2.imshow("Seguimiento de Manos", img)
+    if cv2.waitKey(1) & 0xFF == 27:  # Presiona ESC para salir
+        break
+
+cap.release()
+cv2.destroyAllWindows()
